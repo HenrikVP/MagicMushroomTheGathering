8b22fcdb4f9edda55212e747509f97c2d0b2d2b1
HenrikVP <hpo@tec.dk>
2024-04-03 11:18:54 +0200

    Frokost

--- app/src/main/java/dk/tec/magicmushroomthegathering/MainActivity.java
+++ app/src/main/java/dk/tec/magicmushroomthegathering/MainActivity.java
@@ -1,30 +1,19 @@
 package dk.tec.magicmushroomthegathering;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import android.Manifest;
-import android.os.Looper;
-import android.view.View;
-import android.widget.TextView;
-import com.google.android.gms.location.*;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FusedLocationProviderClient fusedLocationClient;
-    private Location loc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,61 +26,21 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        ((TextView) findViewById(R.id.tv_location)).setText("Hello World");
-
         ActivityResultLauncher<String[]> locationPermissionRequest =
                 registerForActivityResult(new ActivityResultContracts
                                 .RequestMultiplePermissions(), result -> {
-                            Boolean fineLocationGranted = result.getOrDefault(
+                     Boolean fineLocationGranted = result.getOrDefault(
                                     Manifest.permission.ACCESS_FINE_LOCATION, false);
-                            Boolean coarseLocationGranted = result.getOrDefault(
-                                    Manifest.permission.ACCESS_COARSE_LOCATION, false);
                             if (fineLocationGranted != null && fineLocationGranted) {
-                                // Precise location access granted.
-                            } else if (coarseLocationGranted != null && coarseLocationGranted) {
-                                // Only approximate location access granted.
-                            } else {
-                                // No location access granted.
+                                startActivity(new Intent(MainActivity.this, OsmActivity.class));
                             }
                         }
                 );
 
         locationPermissionRequest.launch(new String[]{
                 Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
         });
-
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        findViewById(R.id.btn_map).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, OsmActivity.class));
-            }
-        });
-        startLocationUpdates();
-    }
-
-    private void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //getPermissions();
-            return;
-        }
-        LocationRequest locationRequest = new LocationRequest
-                .Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).build();
-        fusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback() {
-            public void onLocationResult(@NonNull LocationResult locationResult) {
-
-                for (Location location : locationResult.getLocations()) {
-                    loc = location;
-                    ((TextView) findViewById(R.id.tv_location)).setText(
-                            "Long: " + loc.getLongitude() +
-                                    "\nLat: " + loc.getLatitude() +
-                                    "\nSpeed: " + loc.getSpeed() * 60*60/1000
-                    );
-                }
-            }
-        }, Looper.getMainLooper());
-
     }
 }
--- app/src/main/java/dk/tec/magicmushroomthegathering/OsmActivity.java
+++ app/src/main/java/dk/tec/magicmushroomthegathering/OsmActivity.java
@@ -1,44 +1,34 @@
 package dk.tec.magicmushroomthegathering;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
-import android.os.Looper;
-import android.preference.PreferenceManager;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.Priority;
 
 import org.osmdroid.api.IMapController;
-import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
-import java.util.ArrayList;
-
 public class OsmActivity extends AppCompatActivity{
-    private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
+
     private MapView map = null;
+    private IMapController mapController;
     private FusedLocationProviderClient fusedLocationClient;
     private Location loc;
 
+    private Boolean startup = false;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,135 +41,40 @@ public class OsmActivity extends AppCompatActivity{
             return insets;
         });
 
-        //handle permissions first, before map is created. not depicted here
-
-        //load/initialize the osmdroid configuration, this can be done
-        Context ctx = getApplicationContext();
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-        //setting this before the layout is inflated is a good idea
-        //it 'should' ensure that the map has a writable location for the map cache, even without permissions
-        //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
-        //see also StorageUtils
-        //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's
-        //tile servers will get you banned based on this string
-
-        //inflate and create the map
         setContentView(R.layout.activity_osm);
-
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
-        map.setBuiltInZoomControls(true);
-        map.canZoomIn();
-        map.canZoomOut();
         map.setMultiTouchControls(true);
-
-        String[] perm = { Manifest.permission.ACCESS_FINE_LOCATION,
-                // WRITE_EXTERNAL_STORAGE is required in order to show the map
-                Manifest.permission.WRITE_EXTERNAL_STORAGE};
-
-        requestPermissionsIfNecessary(perm);
+        mapController = map.getController();
+        mapController.setZoom(18.0);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        startLocationUpdates();
+        getLocation();
         map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        //this will refresh the osmdroid configuration on resuming.
-        //if you make changes to the configuration, use
-        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        //Configuration.getInstance().save(this, prefs);
-        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+         map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (int i = 0; i < grantResults.length; i++) {
-            permissionsToRequest.add(permissions[i]);
-        }
-        if (permissionsToRequest.size() > 0) {
-            ActivityCompat.requestPermissions(
-                    this,
-                    permissionsToRequest.toArray(new String[0]),
-                    REQUEST_PERMISSIONS_REQUEST_CODE);
-        }
-    }
-
-    private void requestPermissionsIfNecessary(String[] permissions) {
-        ArrayList<String> permissionsToRequest = new ArrayList<>();
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission)
-                    != PackageManager.PERMISSION_GRANTED) {
-                // Permission is not granted
-                permissionsToRequest.add(permission);
-            }
-        }
-        if (permissionsToRequest.size() > 0) {
-            ActivityCompat.requestPermissions(
-                    this,
-                    permissionsToRequest.toArray(new String[0]),
-                    REQUEST_PERMISSIONS_REQUEST_CODE);
-        }
-    }
-
-    private void startLocationUpdates() {
+    private void getLocation() {
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            //getPermissions();
             return;
         }
-        LocationRequest locationRequest = new LocationRequest
-                .Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).build();
-        fusedLocationClient.requestLocationUpdates(locationRequest, new LocationCallback() {
-            public void onLocationResult(@NonNull LocationResult locationResult) {
+        fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
+                .addOnSuccessListener(location -> centerMapOnLocation(location));
+    }
 
-                for (Location location : locationResult.getLocations()) {
-                    loc = location;
-                    IMapController mapController = map.getController();
-                    mapController.setZoom(18.0);
-                    GeoPoint startPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
-                    mapController.setCenter(startPoint);
-//                    ((TextView) findViewById(R.id.tv_location)).setText(
-//                            "Long: " + loc.getLongitude() +
-//                                    "\nLat: " + loc.getLatitude() +
-//                                    "\nSpeed: " + loc.getSpeed() * 60*60/1000
-//                    );
-                }
-            }
-        }, Looper.getMainLooper());
-
+    void centerMapOnLocation(Location location){
+        GeoPoint startPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
+        mapController.setCenter(startPoint);
     }
 }
-
-
-
-
-//import android.os.Bundle;
-//
-//import androidx.activity.EdgeToEdge;
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.core.graphics.Insets;
-//import androidx.core.view.ViewCompat;
-//import androidx.core.view.WindowInsetsCompat;
-//
-//public class OsmActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-
-//    }
-//}
